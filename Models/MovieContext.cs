1a10b98ec1bca09f0b443114178a4771fdc56afb
ColeC <colecannonschool@gmail.com>
2018-11-21 20:23:02 -0700

    Initial check-in of module MvcMovie

---
+++ Models/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
